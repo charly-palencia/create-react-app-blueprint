a7000bc315219dafbc0ae748681cf6dd63f607c9
Charly Palencia <charly@fullstacklabs.co>
2019-06-09 14:06:43 -0500

    clean up

--- test2/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { connectRouter } from "connected-react-router";
-import history from "../history";
-import color from "./color";
-
-export default combineReducers({
-  color,
-  router: connectRouter(history)
-});
