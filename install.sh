f1d2cb062bd9cba9d616c931e08d3be4e5d9223e
charly palencia <charlypalenciay@gmail.com>
2019-06-09 12:27:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 ROOT_PATH =$(pwd)
 FILE1=$1
+echo $FILE1
+echo "$ROOT_PATH/$FILE1"
 rm -rf /var/tmp/create-react-app-blueprint
 git clone https://github.com/charly-palencia/create-react-app-blueprint.git  /var/tmp/create-react-app-blueprint
 cd /var/tmp/create-react-app-blueprint
-echo "$ROOT_PATH/$FILE1"
 #./main.sh "$ROOT_PATH/$FILE1"
 cd -
