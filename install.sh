22aa37f6022a01c7ac8e13af684f4ad82a057a3e
charly palencia <charlypalenciay@gmail.com>
2019-06-09 13:02:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,12 @@
 project_folder="${1}"
 dir=`pwd`
 app_path="${dir}/${project_folder}"
-echo "==> ${app_path}"
- 
+#Remove previous version  
+rm -rf /var/tmp/create-react-app-blueprint
+#Clone latest version  
+git clone https://github.com/charly-palencia/create-react-app-blueprint.git  /var/tmp/create-react-app-blueprint
+#go to tmp folder
+cd /var/tmp/create-react-app-blueprint
+#Install CRA blueprint
+./main.sh "$app_path"
+cd -
